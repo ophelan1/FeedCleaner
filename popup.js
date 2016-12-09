f6d4ec26211f99097987ee316e732bec25ec889f
Owen Phelan <ophelan1@nd.edu>
2016-12-09 06:29:07 -0500

    More stuff working

---
+++ popup.js
@@ -0,0 +1,8 @@
+function hello() {
+	console.log("RUNNING HELLO");
+  chrome.tabs.executeScript({
+    file: '/content.js'
+  }); 
+}
+
+document.getElementById('clickme').addEventListener('click', hello)
