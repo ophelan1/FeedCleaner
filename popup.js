7aa08d87bd0a4c65383f3291e457ffd6b304e1eb
Owen Phelan <ophelan1@nd.edu>
2016-12-12 13:11:38 -0500

    Moved and deleted stuff

--- popup.js
+++
@@ -1,8 +0,0 @@
-function hello() {
-	console.log("RUNNING HELLO");
-  chrome.tabs.executeScript({
-    file: "/content.js"
-  }); 
-}
-
-document.getElementById('clickme').addEventListener('click', hello)
